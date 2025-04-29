0b52cb17d85cee69c6ea248ccdc23f2c93fbd88c
SarahDomscheit <sarri91@googlemail.com>
2025-04-29 21:08:44 +0200

    header desktop version done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,37 @@
 import styles from "./header.module.css";
 import useUserStore from "../../hooks/userStore";
+import Searchbar from "../Searchbar/Searchbar";
 
 const Header = () => {
   const currentUser = useUserStore((state) => state.currentUser);
   return (
-    <div>
-      <img className={`${styles.imgLogo}`} src="/assets/logo.svg" alt="logo" />
-      <div>
-        {currentUser ? (
-          <p>Hallo</p>
-        ) : (
-          <img
-            className={`${styles.imgLogo}`}
-            src="/assets/user-solid.svg"
-            alt="userlogo"
-          />
-        )}
+    <>
+      <div className={`${styles.header}`}>
+        <img
+          className={`${styles.imgLogo}`}
+          src="/assets/logo.svg"
+          alt="logo"
+        />
+        <div>
+          {currentUser ? (
+            <p>Eingeloggt</p>
+          ) : (
+            <div className={`${styles.login}`}>
+              {" "}
+              <img
+                className={`${styles.imgLogin}`}
+                src="/assets/user-solid.svg"
+                alt="userlogo"
+              />
+              <p className={`${styles.loginFont}`}>Log In</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className={`${styles.searchbar}`}>
+        <Searchbar />
+      </div>
+    </>
   );
 };
 export default Header;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,30 @@
+import styles from "./searchbar.module.css";
+const Searchbar = () => {
+  return (
+    <div className={`${styles.wrapper}`}>
+      <form className={`${styles.form}`} action="">
+        {" "}
+        <div className={`${styles.searchBarContainer}`}>
+          <img
+            className={`${styles.imgSearchbar}`}
+            src="/assets/magnifying-glass-solid.svg"
+            alt="search"
+          />
+          <input
+            className={`${styles.searchbar}`}
+            type="text"
+            id="search"
+            placeholder="What are you looking for?"
+          />{" "}
+        </div>
+        <select name="city" id="city" className={`${styles.selectSearchbar}`}>
+          <option value="">City</option>
+          <option value="">Hamburg</option>
+        </select>
+        <button className={`${styles.buttonSearchbar}`}>Find</button>
+      </form>
+    </div>
+  );
+};
+
+export default Searchbar;
