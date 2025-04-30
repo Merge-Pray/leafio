55b4d7f02bbf7e7a49f34454f47f6d9d92577868
benNurtjipta <ben@nurtjipta.de>
2025-04-30 14:15:12 +0200

    zustand persist added

--- src/hooks/userStore.js
+++ src/hooks/userStore.js
@@ -1,15 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  currentUser: null,
-
-  setCurrentUser: (user) => {
-    set({ currentUser: user });
-  },
-
-  clearCurrentUser: () => {
-    set({ currentUser: null });
-  },
-}));
+const useUserStore = create(
+  persist(
+    (set) => ({
+      currentUser: null,
+      setCurrentUser: (user) => set({ currentUser: user }),
+      clearUser: () => set({ currentUser: null }),
+    }),
+    {
+      name: "user-storage", // name of item in localStorage
+      getStorage: () => localStorage, // default is localStorage
+    }
+  )
+);
 
 export default useUserStore;
