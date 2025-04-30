5e00526a3be3321e524d0838cab953fa14c1f9ea
benNurtjipta <ben@nurtjipta.de>
2025-04-30 09:48:33 +0200

    register page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Landingpage from "./pages/Landingpage/Landingpage";
 import Login from "./pages/Login/Login";
 import About from "./pages/About/About";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landingpage />} />
         <Route path="login" element={<Login />} />
         <Route path="about" element={<About />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
