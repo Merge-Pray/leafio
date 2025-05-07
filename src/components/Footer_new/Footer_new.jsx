7b99d5299e4bd72e4c893b15f47ef1b3e16bc72c
cmgoersch <goersch@madways.de>
2025-05-07 12:36:53 +0200

    sbout seite gebastelt

--- src/components/Footer_new/Footer_new.jsx
+++ src/components/Footer_new/Footer_new.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
 import styles from "./footer_new.module.css";
 
 const Footer_new = () => {
   return (
     <footer className={styles.footer}>
-      <p className={styles.text}>
-        Leafio ist ein Projekt im Rahmen des Web Development Kurses beim DCI
-        (April/Mai 2025) und nicht für den produktiven Betrieb gedacht.
-      </p>
+      <div>
+        <p className={styles.text}>
+          Leafio ist ein Projekt im Rahmen des Web Development Kurses beim DCI
+          (April/Mai 2025) und nicht für den produktiven Betrieb gedacht.
+        </p>
+        <div className={styles.linkRow}>
+          <Link to="/about" className={styles.footerLink}>
+            Über Leafio
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
