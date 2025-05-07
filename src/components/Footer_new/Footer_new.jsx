40af4fd1e0947d2109c8e778017c6ad31bce01ee
SarahDomscheit <sarri91@googlemail.com>
2025-05-07 10:18:59 +0200

    buttons gestyled

---
+++ src/components/Footer_new/Footer_new.jsx
@@ -0,0 +1,14 @@
+import styles from "./footer_new.module.css";
+
+const Footer_new = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>
+        Leafio ist ein Projekt im Rahmen des Web Development Kurses beim DCI
+        (April/Mai 2025) und nicht für den produktiven Betrieb gedacht.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer_new;
