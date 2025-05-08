8dd2ca8f5c8ae652e8896b42139b9d6f9a060834
cmgoersch <goersch@madways.de>
2025-05-08 15:34:09 +0200

    splashseen

---
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,22 @@
+// src/components/SplashScreen/SplashScreen.jsx
+import { useEffect } from "react";
+import styles from "./SplashScreen.module.css";
+import logo from "/assets/logo_w.svg"; // Pfad anpassen!
+
+const SplashScreen = ({ onFinish }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onFinish();
+    }, 1100); // 0.8s Animation + 0.3s Fadeout
+
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
+  return (
+    <div className={styles.splashOverlay}>
+      <img src={logo} alt="Logo" className={styles.logo} />
+    </div>
+  );
+};
+
+export default SplashScreen;
